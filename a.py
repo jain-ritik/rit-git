bca4d829dbef4ddb7b9847e65e831d4cd367588e
jain-ritik <44414761+jain-ritik@users.noreply.github.com>
2018-10-29 12:07:07 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL="http://www.mca.gov.in/mcafoportal/showCheckCompanyName.do"
+payload = {'name1':'Google','checkCompanyName_0':'Search'}
+with requests.session() as s:
+    s.headers={"User-Agent":"Mozilla/5.0"}
+    s.post(URL,data=payload)
+    res=s.get("http://www.mca.gov.in/mcafoportal/checkCompanyName.do")
+    soup=BeautifulSoup(res.text,"lxml")
+    print(soup.prettify())
+
